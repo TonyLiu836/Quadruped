e8a930d6dc7678388b95f568f918ccbb2e6a151e
Tony Liu <31741598+TonyLiu836@users.noreply.github.com>
2022-08-16 10:38:11 -0500

    Everything works (not integrated with hardware)

--- hardware_interface/include/hw_interface(original).h
+++ hardware_interface/include/hw_interface(original).h
@@ -1,33 +1,23 @@
 #ifndef HW_INTERFACE_H
 #define HW_INTERFACE_H
-#define FREQUENCY 50
 
 #include<ros/ros.h>
 #include<string>
 #include<trajectory_msgs/JointTrajectory.h>
 #include<sensor_msgs/JointState.h>
-#include "../PCA9685Lib/PCA9685.h"
+
 //extern PCA9685 pwm;
 
 template<typename PublishT, typename SubscribeT>  //declare types for the messages of the publisher and subscriber
 
 class Hw_interface{
 public:
-    Hw_interface(){
-        
-    }
+    Hw_interface(){}
     //Hw_interface(sensor_msgs::JointState publishTopicName, trajectory_msgs::JointTrajectory subscribeTopicName, int queueSize){
     Hw_interface(std::string publishTopicName, std::string subscribeTopicName, int queueSize)
     {
         publisherObj = nH.advertise<PublishT>(publishTopicName, queueSize);
         subscriberObj = nH.subscribe<SubscribeT>(subscribeTopicName, queueSize, &Hw_interface::subscriberCallback, this);   
-        
-        robot_state.position.resize(12);
-        robot_state.velocity.resize(12);
-        robot_state.effort.resize(12);
-        robot_state.name = {"motor_front_left_hip", "motor_front_left_upper_leg", "motor_front_left_lower_leg", "motor_front_right_hip", "motor_front_right_upper_leg", "motor_front_right_lower_leg", "motor_back_left_hip", "motor_back_left_upper_leg", "motor_back_left_lower_leg", "motor_back_right_hip", "motor_back_right_upper_leg", "motor_back_right_lower_leg"};
-        pwm.init(1, 0x40);
-        pwm.setPWMFreq(FREQUENCY);
     }
     void subscriberCallback(const typename SubscribeT::ConstPtr& receivedMsg);
 
@@ -35,8 +25,7 @@ protected:
     ros::Subscriber subscriberObj;
     ros::Publisher publisherObj;
     ros::NodeHandle nH;
-    sensor_msgs::JointState robot_state;
-    PCA9685 pwm;
+
 };
 
 #endif
--- hardware_interface/src/hw_interface(original).cpp
+++ hardware_interface/src/hw_interface(original).cpp
@@ -1,9 +1,12 @@
+//hardware interface between RPi and PCA9685 to control servo motors
+
 #include <ros/ros.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <sensor_msgs/JointState.h>
 //#include <PiPCA9685/PCA9685.h>
 
 // Includes for PCA9685
+#include "../PCA9685Lib/PCA9685.h"
 #include <iostream>
 #include <stdio.h>
 #include <thread>         
@@ -18,19 +21,24 @@
 
 #define MIN_PULSE_WIDTH 900
 #define MAX_PULSE_WIDTH 2100
+#define FREQUENCY 50
 
 using namespace std;
 
 int offset = 0;
 
+//robot joint state
+sensor_msgs::JointState robot_state;
+
 //const char* joint_name[16] = {"back_left_foot", "back_left_hip", "back_left_lower_leg", "back_left_upper_leg", "back_right_foot","back_right_hip", "back_right_lower_leg", "back_right_upper_leg", "front_left_foot","front_left_hip", "front_left_lower_leg", "front_left_upper_leg", "front_right", "front_right_hip","front_right_lower_leg","front_right_upper_leg"};
-
-//const char* joint_names[12] = {"motor_front_left_hip", "motor_front_left_upper_leg", "motor_front_left_lower_leg", "motor_front_right_hip", "motor_front_right_upper_leg", "motor_front_right_lower_leg", "motor_back_left_hip", "motor_back_left_upper_leg", "motor_back_left_lower_leg", "motor_back_right_hip", "motor_back_right_upper_leg", "motor_back_right_lower_leg"};
-
-double pos[12];
+const char* joint_names[12] = {"motor_front_left_hip", "motor_front_left_upper_leg", "motor_front_left_lower_leg", "motor_front_right_hip", "motor_front_right_upper_leg", "motor_front_right_lower_leg", "motor_back_left_hip", "motor_back_left_upper_leg", "motor_back_left_lower_leg", "motor_back_right_hip", "motor_back_right_upper_leg", "motor_back_right_lower_leg"};
+//robot_state.name = joint_names;
+float pos[12];
 double currPos[12];
-//float vel[12];
-//float eff[12];
+float vel[12];
+float eff[12];
+
+
 
 //Declaration of Functions used ==================================
 void pwmwrite(int& angle, PCA9685 pwm, size_t& channel);
@@ -64,7 +72,6 @@ void pwmwrite(double& angle, PCA9685 pwm, size_t& channel){
     }
     
     val = angleToAnalog(angle);
-
     //not sure what offset does
     val += offset;
 
@@ -72,27 +79,51 @@ void pwmwrite(double& angle, PCA9685 pwm, size_t& channel){
     //channel: The channel that should be updated with the new values (0..15)
     //on: The tick (between 0..4095) when the signal should transition from low to high
     //off:the tick (between 0..4095) when the signal should transition from high to low
+    
     pwm.setPWM(channel,0,val);
+    //usleep(30);
     //cout << "Channel: " << channel << "\tSet to angle: " << angle << "\tVal: " << val << endl;
     //return(0);
 }
 
 
 
+
 template<>
 void Hw_interface<sensor_msgs::JointState, trajectory_msgs::JointTrajectory>::subscriberCallback(const trajectory_msgs::JointTrajectory::ConstPtr& receivedMsg){
+    //ROS_INFO("I received the following: %s", receivedMsg->joint_names);
+    //ROS_INFO("Sending the received message on 'joint state' topic");
+    //std_msgs::String echo_msg;    
+    //echo_msg.data = receivedMsg->data;
+    //publisherObj.publish(echo_msg);
+    
+    PCA9685 pwm;
+    pwm.init(1,0x40);
+    pwm.setPWMFreq(FREQUENCY); 
+    //trajectory_msgs::JointTrajectory robot_traj = receivedMsg->data;
+    
+    //set the current position to the newest incoming trajectory 
+    //currPos = receivedMsg->joint_trajectory->points->positions;
+    //ROS_INFO("size of points array: ", receivedMsg->points.size());
 
-    //set the current position to the newest incoming trajectory     
     for(size_t i=0; i < 12; ++i){
+        //currPos[i] = receivedMsg->points[i].positions[0];
         currPos[i] = receivedMsg->points[0].positions[i];    
     }
-   
+    //currPos = receivedMsg->points;
+    
     //command the 12 joints to move to desired positoins
     for (size_t ind=0; ind<12; ++ind){
         pwmwrite(currPos[ind], pwm, ind);
         pos[ind] = currPos[ind];
     }
     
+    //robot_state.name_length = 12;
+    //robot_state.velocity_length = 12;
+    //robot_state.effort_length = 12;
+
+    //robot_state.header.stamp = nH.now();
+    //robot_state.header.frame_id = "dog";
     
     for(size_t ind=0; ind <12; ++ind){
         //robot_state.name[ind] = joint_names[ind];
@@ -101,16 +132,18 @@ void Hw_interface<sensor_msgs::JointState, trajectory_msgs::JointTrajectory>::su
         //robot_state.effort[ind] = eff[ind];
     }
     
-    publisherObj.publish(robot_state);      
+    publisherObj.publish(robot_state);   
 }
-
 
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "hw_interface");
     
+    //Hw_interface<sensor_msgs::JointState, trajectory_msgs::JointTrajectory> hwInterface("joint_states", "joint_group_position_controller/command", 1000);
+    
     Hw_interface<sensor_msgs::JointState, trajectory_msgs::JointTrajectory> hwInterface("joint_states", "joint_group_position_controller/command", 1000);
     
+    //Hw_interface<sensor_msgs::JointState, trajectory_msgs::JointTrajectory> hwInterface(, , 100);
     ros::spin();
 }
 
