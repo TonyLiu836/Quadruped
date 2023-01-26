8e0283fe1cfdbd533677675801e3ce51d84700c4
Tony Liu <31741598+TonyLiu836@users.noreply.github.com>
2023-01-26 15:09:56 -0600

    fix unpack_msg

--- testCAN2/CANCommLib/CAN_comm_libV2.cpp
+++ testCAN2/CANCommLib/CAN_comm_libV2.cpp
@@ -28,8 +28,8 @@ unsigned int second = 1000000;
 //limits 
 #define P_MIN -12.5f  // -4*pi              #in radians
 #define P_MAX 12.5f   // 4*pi
-#define V_MIN -45.0f
-#define V_MAX 45.0f
+#define V_MIN -65.0f
+#define V_MAX 65.0f
 #define KP_MIN 0.0f
 #define KP_MAX 500.0f
 #define KD_MIN 0.0f
@@ -94,41 +94,35 @@ public:
         bind(s, (struct sockaddr *)&addr, sizeof(addr));
     }
 
-    void pack_msg(can_frame& frame, float pos, float vel, float KP, float KD, float T_FF){
-    
-        //std::cout << frame << std::endl;
-    
-        int pos_int = float_to_uint(pos, P_MIN,P_MAX, 16);
-        int vel_int = float_to_uint(vel, V_MIN, V_MAX, 12);
-        int KP_int = float_to_uint(KP, KP_MIN, KP_MAX, 12);
-        int KD_int = float_to_uint(KD, KD_MIN, KD_MAX, 12);
-        int T_FF_int = float_to_uint(T_FF, T_MIN, T_MAX, 12);
 
-        frame.data[0] = pos_int >> 8;                                       //pos[15~8]
-        frame.data[1] = pos_int & 0xFF;                                     //pos[7~0]
-        frame.data[2] = vel_int >> 4;                                       //vel[11~4]
-        frame.data[3] = ((vel_int & 0xF) << 4) |(KP_int >> 8);              //vel [3~0] | KP [11~8]
-        frame.data[4] = KP_int & 0xFF;                                      //KP[7~0]
-        frame.data[5] = KD_int >> 4;                                        //KD[11~8]
-        frame.data[6] = ((KD_int & 0xF) << 4) | (T_FF_int >> 8);            //KD[3~0] | FF[11~8]
-        frame.data[7] = T_FF_int & 0xff;                                    //FF[7~0]
-    }   
-
-    void unpack_msg(std::vector<int> data){
-
-        int pos_int = data[0] << 8 | data[1];
-        int vel_int = data[2] << 4 | data[3] >> 4;
-        int kp_int = (data[3] & 0x0F) << 8 | data[4];
-        int kd_int = data[5] << 4 | data[6] >> 4;
-        int FF_int = (data[6] & 0x0F) << 8 | data[7];
+    // void unpack_msg(std::vector<int> data){
         
+    //     int pos_int = (data[0] << 8) | data[1];
+    //     int vel_int = (data[2] << 4) | (data[3] >> 4);
+    //     int kp_int = ((data[3] & 0xF) << 8) | data[4];
+    //     int kd_int = (data[5] << 4) | (data[6] >> 4);
+    //     int FF_int = ((data[6] & 0xF) << 8) | data[7];
+    //     // std::cout << "pos_int= " << pos_int << " | vel_int= " << vel_int << " | kp_int= " << kp_int << " | kd_int= " << kd_int << " | FF_int= " << FF_int << std::endl ;
+
+    //     p_des = uint_to_float(pos_int, P_MIN, P_MAX, 16);
+    //     v_des = uint_to_float(vel_int, V_MIN, V_MAX, 12);
+    //     kp = uint_to_float(kp_int, KP_MIN, KP_MAX, 12);
+    //     kd = uint_to_float(kd_int, KD_MIN, KD_MAX, 12);
+    //     t_ff = uint_to_float(FF_int, T_MIN, T_MAX, 12);
+        
+    //     std::cout << "pos= " << p_des << " vel= " << v_des << " kp= " << kp << " kd= " << kd << " FF= " << t_ff << std::endl;
+    // }
+    
+    void unpack_msg(std::vector<int> data){
+        int pos_int = (data[1] << 8) | data[2];
+        int vel_int = (data[3] << 4) | (data[4] >> 4);
+        int FF_int = ((data[4] & 0xF) << 8) | data[5];
+
         p_des = uint_to_float(pos_int, P_MIN, P_MAX, 16);
         v_des = uint_to_float(vel_int, V_MIN, V_MAX, 12);
-        kp = uint_to_float(kp_int, KP_MIN, KP_MAX, 12);
-        kd = uint_to_float(kd_int, KD_MIN, KD_MAX, 12);
         t_ff = uint_to_float(FF_int, T_MIN, T_MAX, 12);
-        
-        std::cout << "pos= " << p_des << " vel= " << v_des << " kp= " << kp << " kd= " << kd << " FF= " << t_ff << std::endl;
+
+        std::cout << "pos= " << p_des << " | vel= " << v_des << " | FF= " << t_ff << std::endl;
     }
 
     void enter_motor_mode(){
@@ -149,16 +143,26 @@ public:
         frame.data[7] = 0xFC;
         
         nbytes = write(s, &frame, sizeof(struct can_frame));
+        
 
         std::cout << "Motor Mode Enabled!" << std::endl; 
         
         int nbytes2 = recvfrom(s, &frame, sizeof(struct can_frame), addr.can_ifindex, (struct sockaddr*)&addr, &len);
-
+                        
         std::vector<int> recv_data;
         for (int i = 0; i < sizeof(frame.data)/sizeof(frame.data[0]); i++){
             recv_data.push_back((int)frame.data[i]);
         }
         
+        
+        std::cout << "nytes = " << nbytes << std::endl;
+        std::cout << "nytes2 = " << nbytes2 << std::endl;   
+        std::cout << "size of recv_data = " << recv_data.size() << std::endl;
+
+        for (int i: recv_data){
+            std::cout << i << " "<< std::endl;
+        }
+
         unpack_msg(recv_data);
     }
 
@@ -220,10 +224,29 @@ public:
         for (int i = 0; i < sizeof(frame.data)/sizeof(frame.data[0]); i++){
             recv_data.push_back((int)frame.data[i]);
         }
-        
+
         unpack_msg(recv_data);
     }
 
+    void pack_msg(can_frame& frame, float pos, float vel, float KP, float KD, float T_FF){
+    
+        //std::cout << frame << std::endl;
+    
+        int pos_int = float_to_uint(pos, P_MIN,P_MAX, 16);
+        int vel_int = float_to_uint(vel, V_MIN, V_MAX, 12);
+        int KP_int = float_to_uint(KP, KP_MIN, KP_MAX, 12);
+        int KD_int = float_to_uint(KD, KD_MIN, KD_MAX, 12);
+        int T_FF_int = float_to_uint(T_FF, T_MIN, T_MAX, 12);
+
+        frame.data[0] = pos_int >> 8;                                       //pos[15~8]
+        frame.data[1] = pos_int & 0xFF;                                     //pos[7~0]
+        frame.data[2] = vel_int >> 4;                                       //vel[11~4]
+        frame.data[3] = ((vel_int & 0xF) << 4) |(KP_int >> 8);              //vel [3~0] | KP [11~8]
+        frame.data[4] = KP_int & 0xFF;                                      //KP[7~0]
+        frame.data[5] = KD_int >> 4;                                        //KD[11~8]
+        frame.data[6] = ((KD_int & 0xF) << 4) | (T_FF_int >> 8);            //KD[3~0] | FF[11~8]
+        frame.data[7] = T_FF_int & 0xff;                                    //FF[7~0]
+    }  
 
     void write_can_frame(float pos, float vel, float KP, float KD, float FF){
         
@@ -232,11 +255,11 @@ public:
         //write CAN frame
         struct can_frame frame;
         
-        frame.can_id  = motor_id;          //motor id
+        frame.can_id  = motor_id;        //motor id
         frame.can_dlc = 8;              //len of data 
 
         pack_msg(frame, pos, vel, KP, KD, FF);
-        
+
         nbytes = write(s, &frame, sizeof(struct can_frame));
         int nbytes2 = recvfrom(s, &frame, sizeof(struct can_frame), addr.can_ifindex, (struct sockaddr*)&addr, &len);
         
@@ -244,36 +267,49 @@ public:
         for (int i = 0; i < sizeof(frame.data)/sizeof(frame.data[0]); i++){
             recv_data.push_back((int)frame.data[i]);
         }
+        
+        std::cout << "nytes = " << nbytes << std::endl;
+        std::cout << "nytes2 = " << nbytes2 << std::endl;   
+        std::cout << "size of recv_data = " << recv_data.size() << std::endl;
+
+        for (int i: recv_data){
+            std::cout <<"received Data = " << i << " ";
+        }
+
         unpack_msg(recv_data);
     }
 };
 
+
+//purple actuator = M2
+//yellow actuator = M1 
 //used to test functions above
 int main(){
     //setup CAN
     const char *interface_name = "can0";           
     open_port(interface_name, 1000000);    
     
-    // MotorController M1 {interface_name, 1};
-    MotorController M2 {interface_name, 2};
+    MotorController M1 {interface_name, 1};
+    // MotorController M2 {interface_name, 2};
 
     // MotorController M2 {}
-    // M1.enter_motor_mode();
-    M2.enter_motor_mode();
+    M1.enter_motor_mode();
+    // M2.enter_motor_mode();
 
-    // M1.zero_position_sensor();
-    M2.zero_position_sensor();
+    M1.zero_position_sensor();
+    // M2.zero_position_sensor();
 
     // write_can_frame(POS, VEL, KP, KD, FF);
     // M1.write_can_frame(3.1415,0,3,0.2,0);
-    // usleep(6*second);
-    M2.write_can_frame(0.5233,0,3,0.2,0);          //currently purple actuator
-    // M1.write_can_frame(0,0,0,0,0);
+    M1.write_can_frame(1,1,1,1,1);
+    usleep(6*second);
+    // M2.write_can_frame(0.5233,0,3,0.2,0);          //currently purple actuator
+    M1.write_can_frame(0,0,0,0,0);
     // M1.write_can_frame(1,1,1,1,1);
-    usleep(1*second);
+    // usleep(2*second);
 
-    // M1.exit_motor_mode();
-    M2.exit_motor_mode();
+    M1.exit_motor_mode();
+    // M2.exit_motor_mode();
 
     close_port(interface_name);
 
